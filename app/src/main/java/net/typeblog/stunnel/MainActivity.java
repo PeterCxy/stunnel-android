b0ea0db72bbe624745dfcb7cb4b2b776d18033bf
Peter Cai <xqsx43cxy@126.com>
2015-09-19 17:46:47 +0800

    Initial commit

---
+++ app/src/main/java/net/typeblog/stunnel/MainActivity.java
@@ -0,0 +1,14 @@
+package net.typeblog.stunnel;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
