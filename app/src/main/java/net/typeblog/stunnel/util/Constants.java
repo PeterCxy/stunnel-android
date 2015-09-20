f1dca98159579cbe383cfa914ae4d0cc3ec5f9e7
Peter Cai <xqsx43cxy@126.com>
2015-09-20 09:13:06 +0800

    Set debug = 4 as default

--- app/src/main/java/net/typeblog/stunnel/util/Constants.java
+++ app/src/main/java/net/typeblog/stunnel/util/Constants.java
@@ -10,5 +10,6 @@ public class Constants
 	
 	public static final String DEF_CONFIG = "# Do not change the following lines\n" +
 							"log = overwrite\noutput = " + HOME + LOG + "\n" +
-							"pid = " + HOME + PID + "\n\n# Now add your configs\n";
+							"pid = " + HOME + PID + "\n# Less verbosity, prevent log from getting too long\n# Set debug = 7 if you want more verbosity.\ndebug = 4" +
+							"\n\n# Now add your configs\n";
 }
